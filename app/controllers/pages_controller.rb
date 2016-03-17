7e55454ebd7da3838ec99e846bc8b1bcf3eb7174
benkline <ben.kline@gmail.com>
2016-03-17 16:21:23 -0600

    still trying to wrap my head around erb and views... also tests... also devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,22 @@
 class PagesController < ApplicationController
+=begin
+pages     GET    /pages(.:format)               pages#index
+          POST   /pages(.:format)               pages#create
+new_page  GET    /pages/new(.:format)           pages#new
+edit_page GET    /pages/:id/edit(.:format)      pages#edit
+page      GET    /pages/:id(.:format)           pages#show
+          PATCH  /pages/:id(.:format)           pages#update
+          PUT    /pages/:id(.:format)           pages#update
+          DELETE /pages/:id(.:format)           pages#destroy
+=end
   def index
     @pages = Page.all
   end
 
-  def show
-    @page = Page.find(params[:id])
-  end
-
-  def new
-    @page = Page.new
-  end
-
   def create
-    @page = page.new
-    @page.title = params[:page][:title]
-    @page.body = params[:page][:body]
+    @page = Page.find(params[:page_id])
+    @page = @page.build(page_params)
+    @page.user = current_user
 
     if @page.save
      flash[:notice] = "Page was saved."
@@ -25,10 +27,21 @@ class PagesController < ApplicationController
     end
   end
 
+  def new
+    @page = Page.new
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
   def update
      @page = page.find(params[:id])
-     @page.title = params[:page][:title]
-     @page.body = params[:page][:body]
+     @page.assign_attributes(page_params)
 
      if @page.save
        flash[:notice] = "Page was updated."
@@ -50,4 +63,18 @@ class PagesController < ApplicationController
     render :show
   end
 end
+
+  private
+  def page_params
+    params.require(:page).permit(:title, :body)
+  end
+
+  def authorize_user
+    page = Page.find(params[:id])
+    unless current_user == page.user || current_user.admin?
+      flash[:alert] = "Momma told the doctor and the doctor said - no more monkeys, jumpin on the (admin's) bed."
+      redirect_to [page]
+    end
+  end
+
 end
