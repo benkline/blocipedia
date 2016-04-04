7f2eee5ee6ccd44793545c7928cdd872d8ae4cf9
benkline <ben.kline@gmail.com>
2016-04-04 08:33:25 -0600

    Use faker for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,65 +10,28 @@ require "random_data"
 # Create Users
 5.times do
   User.create!(
-  email:    RandomData.random_email,
-  password: RandomData.random_word
+  email:    Faker::Internet.email,
+  password: Faker::Internet.password,
+  role:  rand(0..2)
   )
 end
 users = User.all
 
-# #create cities
-# 15.times do
-#   Cities.create!(
-#     name: RandomData.random_word,
-#     description: RandomData.random_paragraph
-#     integer: (for enum hash)
-#   )
-# end
-# topics = Citi.all
-
-# #create pages
+# Create Pages
 50.times do
   page = Page.create!(
     user: users.sample,
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph,
+    title: Faker::Book.title,
+    body: Faker::Hipster.paragraph(2),
     private: false
   )
 
-# page.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
-#
-# rand(1..5).times { post.votes.create!(value:[-1,1].sample, user: users.sample) }
-
+ page.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
 end
 
 pages = Page.all
-
-###create comments
-
-# 100.times do
-#   Comment.create!(
-#     user: users.sample,
-#     post: posts.sample,
-#     body: RandomData.random_paragraph
-#   )
-# end
-#
-# admin = User.create!(
-#   name: 'Admin User',
-#   email: 'admin@example.com',
-#   password: 'helloworld',
-#   role: 'admin'
-# )
-#
-# member = User.create!(
-#   name: 'Member User',
-#   email: 'member@example.com',
-#   password: 'helloworld'
-# )
 
 #print results
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Page.count} posts created"
-#puts "#{Citi.count} votes created"
-#puts "#{Vote.count} votes created"
